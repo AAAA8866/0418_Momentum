d465629eb9f24e1e23dc57a7a60717506031ac7f
AAAA8866 <100890250+AAAA8866@users.noreply.github.com>
2022-04-19 08:05:00 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,37 @@
-const title = document.getElementById("title");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+// const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username"
 
-console.dir(document);
-title.innerText = "Got you!";
+function onLoginSubmit(event) {
+    event.preventDefault();    
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+
+    const username = loginInput.value;
+    localStorage.setItem("username", USERNAME_KEY); //username 저장
+
+    paintGreetings(username);
+}
+
+function paintGreetings (username) {
+    greeting.classList.remove(username);
+    greeting.innerText = `hello ${savedUsername}`;
+}
+
+
+loginForm.addEventListener("submit",onLoginSubmit);
+
+const savedUsername = localStorage.getItem("USERNAME_KEY");
+console.log(savedUsername);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("SUBMIT", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
+
+
+
